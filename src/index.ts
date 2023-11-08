bdbe612e21e1a72d2866032ae141eebb1734a8b1
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-11-08 17:23:16 +0530

    feat: add commander as cli interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-console.log("Hello world");
+import { program } from 'commander';
+
+
+// Set the version and description of your CLI app
+program
+    .version('1.0.0')
+    .description('VacationResponder - An automated email response and thread management tool.');
+
+// Define options and commands
+program
+    .command('status')
+    .description('Outputs the currently logged-in Google account')
+    .action(async () => {
+        console.log("TODO: status");
+    });
+
+program
+    .command('logout')
+    .description('Logs out the current user')
+    .action(async () => {
+        console.log("TODO: logout");
+    });
+
+program
+    .command('login')
+    .description('Opens a browser window to OAuth Google account')
+    .action(async () => {
+        console.log("TODO: login");
+    });
+
+program
+    .command('Scheduler')
+    .description('Starts the scheduler of vacation responder which will run at random intervals (ranging from 45 to 120 seconds)')
+    .action(async () => {
+        console.log("TODO: Schedule");
+    });
+
+// Set the default action to trigger the --help function
+program
+    .action(() => {
+        program.help();
+    });
+
+// Parse the arguments and display outputs based on commands
+program.parse(process.argv);
