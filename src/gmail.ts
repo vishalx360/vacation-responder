728283e9aaf3fe4e4bb9fcd44a538f0d0f01a6e5
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-11-08 09:51:43 +0530

    add gmail helper functions

--- src/gmail.ts
+++ src/gmail.ts
@@ -19,3 +19,60 @@ export async function listLabels(auth: Auth.OAuth2Client) {
         console.log(`- ${label.name}`);
     });
 }
+
+export async function sendEmail(auth: Auth.OAuth2Client) {
+    const gmail: gmail_v1.Gmail = google.gmail({ version: 'v1', auth });
+    const res = await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: {
+            raw: 'Hello World!',
+        },
+    });
+    console.log(res.data);
+}
+
+export async function getMessages(auth: Auth.OAuth2Client) {
+    const gmail: gmail_v1.Gmail = google.gmail({ version: 'v1', auth });
+    const res = await gmail.users.messages.list({
+        userId: 'me',
+    });
+    console.log(res.data);
+}
+
+export async function sendEmailAndLabel(auth: Auth.OAuth2Client, labelName: string) {
+    const gmail: gmail_v1.Gmail = google.gmail({ version: 'v1', auth });
+    const res = await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: {
+            raw: 'Hello World!',
+            labelIds: [await createLabel(auth, labelName)] // Include the label ID when sending the email
+        },
+    });
+    console.log(res.data);
+}
+
+export async function createLabel(auth: Auth.OAuth2Client, labelName: string) {
+    const gmail: gmail_v1.Gmail = google.gmail({ version: 'v1', auth });
+    const res = await gmail.users.labels.create({
+        userId: 'me',
+        requestBody: {
+            name: labelName,
+            messageListVisibility: 'show',
+            labelListVisibility: 'labelShow',
+        },
+    });
+    return res.data.id;
+}
+
+export async function moveEmailToLabel(auth: Auth.OAuth2Client, messageId: string, labelId: string) {
+    const gmail: gmail_v1.Gmail = google.gmail({ version: 'v1', auth });
+    const res = await gmail.users.messages.modify({
+        userId: 'me',
+        id: messageId,
+        requestBody: {
+            addLabelIds: [labelId],
+        },
+    });
+    console.log(res.data);
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { authorize } from './auth';
 import { listLabels } from './gmail';
 
+
 (async () => {
     try {
+        console.log("MailAutoResponder")
         const auth = await authorize();
         await listLabels(auth);
+        // console.log(auth);
     } catch (err) {
         console.log(err);
     }
 })();
+
+
